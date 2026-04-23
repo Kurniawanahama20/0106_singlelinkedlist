3850858aa4ddba5261925c1095651aad8822a2a5
Kurniawanahama20 <umykurniawanahama@gmail.com>
2026-04-23 11:36:38 +0700

    pembuataan bool  listEmpty kedalam pemrograman

--- SingleLinkList.cpp
+++ SingleLinkList.cpp
@@ -60,5 +60,22 @@ public:
         previous->next = nodeBaru;
     }
 
-    
+    bool listEmpty()
+    {
+        return (START == NULL);
+    }
+
+    bool search(int nim, Node *&previous, Node *&current)
+    {
+        previous = START;
+        current = START;
+
+        while (current != NULL && nim != current->noMhs)
+        {
+            previous = current;
+            current = current->next;
+        }
+
+        return (current != NULL);
+    }
 };
