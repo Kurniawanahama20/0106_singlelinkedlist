0e4a839606b9779ca9a8077e358889e653485c65
Kurniawanahama20 <umykurniawanahama@gmail.com>
2026-04-23 11:57:36 +0700

    pembuatan int main

--- SingleLinkList.cpp
+++ SingleLinkList.cpp
@@ -115,3 +115,30 @@ public:
         }
     }   
 };
+
+int main()
+{
+    Linkedlist mhs;
+    int nim;
+    char ch;
+    
+    do
+    {
+        cout << "\nMenu" << endl;
+        cout << "1. Menambah data ke dalam list" << endl;
+        cout << "2. Menghapus data dari dalam list" << endl;
+        cout << "3. Menampilkan semua data dalam list" << endl;
+        cout << "4. Mencari data dalam list" << endl;
+        cout << "5. Keluar" << endl;
+
+        cout << "\nMasukkan pilihan (1-5): ";
+        cin >> ch;
+
+        switch (ch)
+        {
+        case '1':
+            mhs.addNote();
+            break;
+        }
+    }
+}
